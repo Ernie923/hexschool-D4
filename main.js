d9ac86b1ea379de235ad2c3de15d0c94ecf38d66
Leo <leo2012.c@gmail.com>
2024-09-24 11:23:24 +0800

    修改 header

--- main.js
+++ main.js
@@ -6,11 +6,5 @@ const navbarToggler = document.querySelector(".navbar-toggler");
 const mainElement = document.querySelector("main");
 
 navbarToggler.addEventListener("click", () => {
-  if (mainElement.style.background === "rgba(0, 0, 0, 0.4)") {
-    console.log("is already black")
-    mainElement.style.background = "";
-  } else {
-    mainElement.style.background = "#00000066";
-    console.log("is not already black")
-  }
+  mainElement.classList.toggle("main-overlay");
 });
