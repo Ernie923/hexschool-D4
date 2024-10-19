01d28979402188800f61079ddb0abb6f3a304074
hanchen <hanchen923@hotmail.com>
2024-10-20 01:19:07 +0800

    index update 2

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
 
 
 // 當漢堡選單下拉時背景顏色變為黑色半透明
@@ -114,8 +114,8 @@ $(document).ready(function () {
   });
 });
 
-const swiper = new Swiper('.swiper', {
-    // configure Swiper to use modules
-    modules: [Navigation, Pagination],
+// const swiper = new Swiper('.swiper', {
+//     // configure Swiper to use modules
+//     modules: [Navigation, Pagination],
     
-});
+// });
